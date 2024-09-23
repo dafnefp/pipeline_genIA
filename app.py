baaeb44c89e648088e26ad9bedb82c189ce491a4
Dafne Fernandes Piovesan <dafne.piovesan@autoglass.com.br>
2024-09-23 17:40:25 -0300

    arrumando o codigo do streamlit

--- app.py
+++ app.py
@@ -1,63 +1,21 @@
 import streamlit as st
-from datetime import date, timedelta
-from contrato import Dados
-from webscraping import obter_codigos #, buscar_dados
-from teste import criar_dados
-from genIA import ask_groq
-from pydantic import ValidationError
+from streamlit_funcoes import busca_acoes, escolher_acao, buscar_informacoes, perguntas
 
 def main():
-
     st.title('Informações Ações da Ibovespa')
 
-    try:
-        #acoes = obter_codigos()
-        acoes = ['BRKM5', 'USIM5', 'CVCB3', 'RENT3']
+    # Passo 1: Inicializa o web scraping para buscar a lista de ações disponíveis
+    busca_acoes()
 
-        escolher_acao(acoes)
+    # Passo 2: Usuário escolhe a ação e seleciona o período das informações
+    escolher_acao()
 
-    except Exception as e:
-        st.error(f"Ocorreu um erro durante ao obter a lista de ações: {str(e)}")
- 
+    # Passo 3: Inicializa o web scraping para buscar as informações dos dados selecionados pelo usuário
+    buscar_informacoes()
 
-def escolher_acao(acoes):
-
-    escolha = st.selectbox('Escolha qual ação deseja obter informações', acoes)
-    data_inicio = st.date_input('Escolha a data inicial', date.today() - timedelta(days=30))
-    data_fim = st.date_input('Escolha a data final', date.today())
-
-    if st.button("Analisar"):
-        try: 
-            validacao = Dados(
-                escolha = escolha, 
-                data_inicio = data_inicio, 
-                data_fim = data_fim
-            )
-            st.write(validacao)
-
-            buscar_informacoes(escolha, data_inicio, data_fim)
-
-        except ValidationError as e:
-            st.error(e)
-
-def buscar_informacoes(escolha, data_inicio, data_fim):
-
-    try:
-        #df = buscar_dados(escolha, data_inicio, data_fim)
-        df = criar_dados()
-
-        st.success("Dados importados com sucesso!")
-        st.dataframe(df)
-
-        question = st.text_input(f"Qual a sua pergunta em relação a {escolha}")
-
-        if st.button("Perguntar"):
-            answer = ask_groq(question, df)
-            st.write(answer)
-    
-    except Exception as e:
-        st.error(f"Ocorreu um erro durante o web scraping: {str(e)}")
-
+    # Passo 4: Pergunta a IA sobre os dados
+    perguntas()
+       
 
 if __name__ == "__main__":
     main()
